235dc1aed58a56525ac451fe0d53811e3eadc47e
Palabola <hodobalazs@gmail.com>
2020-03-27 14:32:44 +0100

    revert fhir

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -69,13 +69,8 @@
       }
     }
 
-    return FHIR.oauth2.ready()
-    //.then(client => client.request("Patient"))
-    .then($('#fname').html("success"))
-    .catch($('#fname').html("exception"));
-
-    //FHIR.oauth2.ready(onReady, onError);
-    //return ret.promise();
+    FHIR.oauth2.ready(onReady, onError);
+    return ret.promise();
 
   };
 
