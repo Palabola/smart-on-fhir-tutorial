986d74cf3bbfd46b55cc131f55e9d914e595cca9
Palabola <hodobalazs@gmail.com>
2020-03-27 10:49:10 +0100

    FHIR import

---
+++ example-smart-app/src/js/fhir_authorize.js
@@ -0,0 +1,9 @@
+import FHIR from "fhirclient"
+
+(function(window){
+FHIR.oauth2.authorize({
+  'client_id': 'YOUR-SMART-HEALTH-IT-CLIENT-ID-HERE',
+  'scope':  'patient/Patient.read patient/Observation.read launch online_access openid profile'
+});
+
+})(window);
